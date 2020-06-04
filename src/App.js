41238c73261c33b3a1a4584ae56590e5219f3700
Ammar Khalid <ammarwarraich74@gmail.com>
2020-06-04 20:45:43 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1> Hello World by Ammar Assignment 2</h1>
+      <h1> Hello World by Ammar</h1>
+      <h2> ASSIGNMENT 2 DONE!!</h2>
     </div>
   );
 }
